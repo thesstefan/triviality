dc272673c058b185b449caa6e4fd8dc796610c09
TheMightyStefan <stefanthefineman@gmail.com>
2018-04-29 17:09:25 +0300

    Add PushButton module, customized version of QPushButton

---
+++ src/PushButton.cpp
@@ -0,0 +1,34 @@
+#include <QPushButton>
+
+#include "PushButton.h"
+
+PushButton::PushButton(QWidget *parent) : QPushButton(parent) {
+    this->setFlat(true);
+    this->setFixedSize(500, 100);
+
+    this->setStyleSheet(
+            "background-color: rgb(16, 16, 16);"
+            "border: 1px solid #191919;"
+            "color: white;"
+            "font-size: 35px;"
+            );
+}
+
+void PushButton::colorize(bool isCorrect) {
+    if (isCorrect)
+        // Colorize the button Green if the answer is correct.
+        this->setStyleSheet(
+                "background-color: rgb(20, 180, 50);"
+                "border: 1px solid #191919;"
+                "color: white;"
+                "font-size: 35px;"
+                );
+    else
+        // Colorize the button Red if the answer is incorrect.
+        this->setStyleSheet(
+                "background-color: rgb(200, 20, 20);"
+                "border: 1px solid #191919;"
+                "color: white;"
+                "font-size: 35px;"
+                );
+}
