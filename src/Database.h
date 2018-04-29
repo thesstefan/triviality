a5ef65f86b5eb0b957a1f87a08f7f822dc684ac6
TheMightyStefan <stefanthefineman@gmail.com>
2018-04-29 17:05:54 +0300

    Add Database module, which reads and manages the Questions extracted from the Resource File

---
+++ src/Database.h
@@ -0,0 +1,29 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <QString>
+#include <QList>
+
+#include "Question.h"
+
+class Database {
+    private:
+        QList<Question> data;
+
+        QString fileName;
+
+        int entries;
+
+    public:
+        Database(const QString& fileName);
+
+        int read();
+
+        int write(const QString& fileName);
+
+        Question getQuestion(int index) const;
+
+        Question getRandomQuestion() const;
+};
+
+#endif
