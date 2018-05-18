6511d33a34cfad0787d96297d98db5fbdb2b3931
Paul92 <paul.gafton@gmail.com>
2018-05-18 21:36:06 +0100

    Using command line parameters for file name

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,15 @@
 #include "file_database.h"
 
 int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        std::cout << "Usage: ./quiz databaseFileName" << std::endl;
+    }
+
     FileDatabase *database;
     
     try {
-        database = new FileDatabase(":/data.txt");
+        database = new FileDatabase(argv[1]);
     } catch (const Exception& exception) {
         std::cout << exception.what() << std::endl;
     }
