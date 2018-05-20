23ef2d19512ae20eee075200844f878f879f942a
TheMightyStefan <stefanthefineman@gmail.com>
2018-05-20 22:03:11 +0300

    Documented Question

--- src/question.h
+++ src/question.h
@@ -1,3 +1,9 @@
+/**
+ * @file question.h
+ *
+ * This module provides the implementation of Question.
+ */
+
 #ifndef QUESTION_H
 #define QUESTION_H
 
@@ -6,6 +12,11 @@
 
 #include "macros.h"
 
+/**
+ * @class Question
+ *
+ * @brief A class implementation of a question-type structure.
+ */
 class Question {
     private:
         QString question;
@@ -14,12 +25,47 @@ class Question {
         int correctAnswerIndex;
 
     public:
+        /**
+         * @brief Constructs the Question.
+         *
+         * @param question Question text.
+         *
+         * @param correctAnswerIndex Index of the correct answer.
+         *
+         * @param answers List of answers.
+         */
         Question(const QString& question, int correctAnswerIndex, const QList<QString>& answers);
 
+        /**
+         * @brief Returns the question text.
+         *
+         * @return Question text
+         */
         QString getQuestion() const;
+
+        /**
+         * @brief Returns an answer.
+         *
+         * @param answerIndex The index of the answer to be returned.
+         *
+         * @return The answer at @p answerIndex.
+         */
         QString getAnswer(int answerIndex) const;
+
+        /**
+         * @brief Returns the correct answer.
+         *
+         * @return The correct answer.
+         */
         QString getCorrectAnswer() const;
 
+        /**
+         * @brief Returns if an answer is correct.
+         *
+         * @param answerIndex The index of the answer to be checked if it's correct.
+         *
+         * @return @b true if the answer at @p answerIndex is correct, @b false otherwise.
+         */
         bool isCorrectAnswer(int answerIndex) const;
 };
 
