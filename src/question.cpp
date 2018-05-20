7f08571b3c2e07fc515aebe0636c16f11f94dddb
TheMightyStefan <stefanthefineman@gmail.com>
2018-05-20 20:53:38 +0300

    Corrected Question constructor definition

--- src/question.cpp
+++ src/question.cpp
@@ -1,6 +1,6 @@
 #include "question.h"
 
-Question::Question(const QString& question, int correctAnswerIndex, QList<Question> answers) {
+Question::Question(const QString& question, int correctAnswerIndex, const QList<Question>& answers) {
     this->setQuestion(question);
 
     for (int answerIndex = 0; answerIndex < ANSWERS_NUMBER; answerIndex++)
