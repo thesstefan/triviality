cd792612636dc032a828a71511e35dd26435c2a0
TheMightyStefan <stefanthefineman@gmail.com>
2018-05-24 22:34:13 +0300

    Documented macros.h

--- src/macros.h
+++ src/macros.h
@@ -1,1 +1,10 @@
+/**
+ * @file macros.h
+ *
+ * This file provides many globally-used constants.
+ */
+
+/**
+ * @brief This constant defines the number of answers contained by a Question.
+ */
 #define ANSWERS_NUMBER 4
