ab83502f8d510bd2de87255e98cb7cf83917d39a
TheMightyStefan <stefanthefineman@gmail.com>
2018-05-28 15:34:07 +0300

    what() is no longer virtual in Exception. Remove EntryReadFail. Document Exception.

--- src/exceptions.cpp
+++ src/exceptions.cpp
@@ -2,26 +2,12 @@
 
 Exception::Exception(const std::string& message) : message(message) {}
 
-OpenFail::OpenFail(const std::string& message) : Exception(message) {}
-
-const char *OpenFail::what() const noexcept {
+const char *Exception::what() const noexcept {
     return this->message.c_str();
 }
+
+OpenFail::OpenFail(const std::string& message) : Exception(message) {}
 
 ReadFail::ReadFail(const std::string& message) : Exception(message) {}
 
-const char *ReadFail::what() const noexcept {
-    return this->message.c_str();
-}
-
-EntryReadFail::EntryReadFail(const std::string& message) : Exception(message) {}
-
-const char *EntryReadFail::what() const noexcept {
-    return this->message.c_str();
-}
-
 OutOfBounds::OutOfBounds(const std::string& message) : Exception(message) {}
-
-const char *OutOfBounds::what() const noexcept {
-    return this->message.c_str();
-}
--- src/exceptions.h
+++ src/exceptions.h
@@ -1,40 +1,81 @@
+/**
+ * @file exceptions.h
+ *
+ * This module provides the inteface of the exceptions used in the program.
+ */
+
 #include <string>
 #include <exception>
 
+/**
+ * @class Exception
+ *
+ * @brief This class stands as the inteface of the exceptions used across the program.
+ *
+ * @note It is derived from std::exception.
+ */
 class Exception : public std::exception {
     protected:
+        /**
+         * @brief The message of the Exception.
+         */
         std::string message;
 
     public:
+        /**
+         * @brief Constructs the Exception, assigning it a message.
+         *
+         * @param message -> The message of the Exception.
+         */
         Exception(const std::string& message);
 
-        virtual const char *what() const noexcept = 0;
+        /**
+         * @brief Returns the message of the Exception.
+         */
+        const char *what() const noexcept;
 };
 
+/**
+ * @class OpenFail
+ *
+ * @brief Exception used to show that a file failed to open.
+ */
 class OpenFail : public Exception {
     public:
+        /**
+         * @brief Constructs OpenFail, assigning it a message.
+         *
+         * @param message -> The message of OpenFail.
+         */
         OpenFail(const std::string &message);
-
-        const char *what() const noexcept;
 };
 
+/**
+ * @class ReadFail
+ * 
+ * @brief Exception used to show that something failed to be read.
+ */
 class ReadFail : public Exception {
     public:
+        /**
+         * @brief Constructs the Exception, assigning it a message.
+         *
+         * @param message -> The message of the ReadFail.
+         */
         ReadFail(const std::string &message);
-
-        const char *what() const noexcept;
 };
 
-class EntryReadFail : public Exception {
-    public:
-        EntryReadFail(const std::string &message);
-
-        const char *what() const noexcept;
-};
-
+/**
+ * @class OutOfBounds
+ *
+ * @brief Exception used to show that something is out of bounds.
+ */
 class OutOfBounds : public Exception {
     public:
+        /**
+         * @brief Constructs the Exception, assigning it a message.
+         *
+         * @param message -> The message of OutOfBounds.
+         */
         OutOfBounds(const std::string &message);
-
-        const char *what() const noexcept;
 };
